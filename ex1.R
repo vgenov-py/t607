b7f10c31cb5eabe621d8390acb0b96ee5320d03a
vgenov-py <genovese.work@protonmail.com>
2022-03-22 22:00:56 +0100

    A

---
+++ ex1.R
@@ -0,0 +1,68 @@
+### EJERCICIO 1:
+
+a <- c(23,3,6,5,22,64,31,20, 22)
+
+b <- c()
+d <- c()
+i <- 1
+while (i <= length(a)) {
+  if (a[i] %% 2 == 0) {
+    b <- append(b, a[i]^2)
+  }
+  i <- i + 1
+}
+i <- 1
+while (i <= length(a)) {
+  if (a[i] %% 2 != 0) {
+    d <- append(d, a[i]^3)
+  }
+  i <- i + 1
+}
+
+
+b <- c()
+d <- c()
+
+
+
+for (v in a) {
+  if (v %% 2 == 0) {
+    b <- append(b, v)
+  } else {
+    d <- append(d, v)
+  }
+}
+
+### EJERCICIO 2:
+
+
+i <- 10
+result <- c()
+while (i <= 100000) {
+  if (i %% 50 == 10) {
+    result <- append(result, i)
+  }
+  i <- i + 1
+}
+length(result)
+
+result <- c()
+
+i <- 10
+while (i <= 100000) {
+  result <- append(result, i)
+  i <- i + 50
+}
+result[1]
+
+seq(10,100000, 50)
+?seq
+rep(10, 10)
+
+#EJERCICIO 3:
+
+b <- c(1,2,3,4)
+b[-2:-3]
+
+
+
