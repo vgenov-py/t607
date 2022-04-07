137265fcaf8c5c96b57cb8007bcbb524196aae08
unknown <genovese.work@protonmail.com>
2022-04-07 20:31:38 +0200

    COR GENRE

--- ex4_movies.R
+++ ex4_movies.R
@@ -5,4 +5,15 @@ data$Year.of.release <- factor(data$Year.of.release)
 data$Genre <- factor(data$Genre)
 colnames(data) <- c("Film", "Genre", "Official_rating", "Audience_rating", "Budget", "Year")
 
-plot <- ggplot(data, aes(data$Official_rating, data$Audience_rating))
+ggplot(data, aes(Official_rating, Audience_rating,color=Genre)) + geom_point()
+
+cor(data$Official_rating, data$Audience_rating)
+cor(data$Official_rating, data$Budget)
+
+# Calcular las correlaciones entre rating oficial y público por género
+
+for (genre in levels(data$Genre)) {
+  print(genre)
+  a <- data[data$Genre == genre,]
+  print(cor(a$Official_reating, a$Audience_rating))
+}
