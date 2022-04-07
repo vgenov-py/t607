e317485dc565177a406dceeb84057536fb2d66cb
unknown <genovese.work@protonmail.com>
2022-04-07 21:59:32 +0200

    LIMPIEZA EXCEL

---
+++ jamon.R
@@ -0,0 +1,12 @@
+data <- read.csv(file.choose(), sep=";", encoding = "UTF-8")
+str(data)
+factor(data$DescripcionArticulo)
+levels(factor(data$TipoArticuloEscandallo))
+levels(factor(data$TipoArticuloEscandallo))
+
+?read.csv
+as.double("4,3")
+grep("4,3", )
+a <- strsplit("4,3", ",")[1]
+str_replace("4,3", ",", ".")
+
